16f8247275dc5e202550cfe688b7b08f626b2ee0
danielulloa <danielulloa1994@gmail.com>
2019-08-26 22:30:55 -0300

    agrega script modelado matlab

---
+++ planta/datalog/modelado.m
@@ -0,0 +1,2 @@
+[y1]=importdata('plancha.dat');
+[t]=importdata('tiempo.dat');
---
+++ planta/gama/04/modelado.m
@@ -0,0 +1,6 @@
+[y1]=importdata('plancha.dat');
+[y2]=importdata('plancha2.dat');
+y=y1
+y=[y y2]
+a1=y[55 460];
+a2=y(508:2000);
